77116a20584c50bb018286e8ef0eb821d97da864
Thiago Lagden <lagden@gmail.com>
2013-10-11 12:07:45 -0300

    git remote add origin git@github.com:lagden/theSwitch.git

---
+++ README.md
@@ -0,0 +1,28 @@
+# jquery.theSwitch.js
+
+jQuery plugin for **customize** the `<input:checkbox>` tag.
+If you are using a Webkit browser, the plugin will not initialize, because doesn't need this.
+
+[See example!!!](https://github.com/lagden/theSwitch/tree/master/example)
+
+## Installation
+
+Install with [Bower](http://bower.io):
+
+```
+bower install --save jquery.theSwitch.js
+```
+
+The component can be used as a Common JS module, an AMD module, or a global.
+
+## Contributors
+
+    project  : theCombo
+    repo age : 5 months
+    active   : 12 days
+    commits  : 28
+    files    : 8
+    authors  :
+       27  Thiago Lagden           96.4%
+        1  Ofelquis Gimenes        3.6%
+
---
+++ jquery.theSwitch.js
@@ -0,0 +1,99 @@
+/**
+ * jquery.theSwitch.js
+ * Thiago Lagden | @thiagolagden | lagden@gmail.com
+ * jQuery plugin
+ */
+
+;(function(window, navigator, document) {
+
+    "use strict";
+
+    var $ = window.jQuery,
+        doc = document,
+        pluginName = "theSwitch",
+        defaults = {
+            theCss: null
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.$span = null;
+        this.opts = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function() {
+            if (this.$element.is(':checkbox'))
+                this.custom();
+        },
+        custom: function() {
+            var span = doc.createElement('span');
+            span.className = this.element.className;
+            this.$span = $(span);
+            if(this.opts.theCss)
+                this.$span.addClass(this.opts.theCss);
+
+            this.$element
+                .css({
+                    "top": 0,
+                    "left": 0,
+                    "opacity": 0,
+                    "position": "absolute",
+                    "width": "100%"
+                })
+                .on('click.' + this._name, {"that": this}, _click);
+
+            this.$element
+                .after(this.$span)
+                .appendTo(this.$span);
+
+            var frm = this.$element.parents('form:eq(0)');
+            if (frm.length === 1)
+                frm.on('reset', {"that": this}, _reset);
+        }
+    };
+
+    // Private methods
+    function _reset(e) {
+        var that = e.data.that;
+        that.element.checked = true;
+        that.$span.removeClass('checked');
+    }
+
+    function _click(e) {
+        var that = e.data.that;
+        if (that.element.checked)
+            that.$span.addClass('checked');
+        else
+            that.$span.removeClass('checked');
+    }
+
+    $.fn[pluginName] = function(options) {
+        var args = arguments;
+        var hasWebkit = Boolean(navigator.userAgent.indexOf('WebKit') > -1);
+        if (hasWebkit === false) {
+            if (options === undefined || typeof options === 'object') {
+                return this.each(function() {
+                    if (!$.data(this, pluginName))
+                        $.data(this, pluginName, new Plugin(this, options));
+                });
+            } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+                var returns;
+
+                this.each(function() {
+                    var instance = $.data(this, pluginName);
+                    if (instance instanceof Plugin && typeof instance[options] === 'function')
+                        returns = instance[options].apply(instance, Array.prototype.slice.call(args, 1));
+                });
+
+                return returns !== undefined ? returns : this;
+            }
+        }
+        return null;
+    };
+
+})(window, navigator, document);
