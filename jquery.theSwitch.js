bd12b6f85e1489c55437105d3db1642c637d9898
Thiago Lagden <lagden@gmail.com>
2014-05-08 15:33:22 -0300

    Add new events

--- jquery.theSwitch.js
+++ jquery.theSwitch.js
@@ -4,13 +4,10 @@
  */
 
 (function(factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
+    if (typeof define === 'function' && define.amd)
         define(['jquery'], factory);
-    } else {
-        // Browser globals
+    else
         factory(jQuery);
-    }
 }(function($) {
 
     'use strict';
@@ -18,9 +15,30 @@
     var pluginName = 'theSwitch',
         defaults = {
             theCss: null
+        };
+
+    // Private Methods
+    var events = {
+        _reset: function() {
+            this.element.checked = true;
+            this.$span.removeClass('checked');
         },
-        prefixes = 'Webkit Moz ms Ms O'.split(' '),
-        docElemStyle = document.documentElement.style;
+        _click: function() {
+            var method = (this.element.checked) ? 'addClass' : 'removeClass';
+            this.$span[method]('checked');
+        },
+        _keyup: function(event) {
+            var keys = [37, 39];
+            if (keys.indexOf(event.which) !== -1) {
+                if ((event.which === 39 && this.element.checked === false) || (event.which === 37 && this.element.checked === true))
+                    this.$element.trigger('click.' + this._name);
+            }
+        },
+        _toggle: function(event) {
+            var method = (event.type === 'focus') ? 'addClass' : 'removeClass';
+            this.$span[method]('focus');
+        }
+    };
 
     function Plugin(element, options) {
         this.element = element;
@@ -29,29 +47,29 @@
         this.opts = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
-        this.proxy = {};
         this.init();
     }
 
     Plugin.prototype = {
         init: function() {
-            if (this.$element.is(':checkbox'))
+            if (this.$element.is(':checkbox')) {
+                this.element.setAttribute('tabIndex', -1);
                 this.custom();
+            }
         },
         custom: function() {
             // Create the custom element
             var span = document.createElement('span');
             span.className = this.element.className;
+            span.setAttribute('tabIndex', 0);
 
             // Instance jQuery
             this.$span = $(span);
+            this.$span.on('keyup.' + this._name, this.proxy(events._keyup));
             if (this.opts.theCss)
                 this.$span.addClass(this.opts.theCss);
 
-            this.proxy.clica = $.proxy(_onClick, this);
-            this.proxy.reseta = $.proxy(_onReset, this);
-
-            // Some style and listener click
+            // Some styles and listener
             this.$element
                 .css({
                     'top': 0,
@@ -60,52 +78,51 @@
                     'position': 'absolute',
                     'width': '100%'
                 })
-                .on('click.' + this._name, this.proxy.clica)
+                .on('click.' + this._name, this.proxy(events._click))
+                .on('focus.' + this._name + ' blur.' + this._name, this.proxy(events._toggle))
                 .after(this.$span)
                 .appendTo(this.$span);
 
             // Listener Form Reset
             var frm = this.$element.closest('form');
             if (frm.length === 1)
-                frm.on('reset', this.proxy.reseta);
+                frm.on('reset.' + this._name, this.proxy(events._reset));
 
             // Verify initial status
-            this.proxy.clica();
+            this.proxy(events._click)(null);
+
+            // Another way to call a private method
+            // events._click.bind(this)(null);
+        },
+        proxy: function(m) {
+            var that = this;
+            return function() {
+                m.apply(that, arguments);
+            };
         }
     };
 
-    // Private methods
-    function _onReset(event) {
-        this.element.checked = true;
-        this.$span.removeClass('checked');
-    }
-
-    function _onClick(event) {
-        var method = (this.element.checked) ? 'addClass' : 'removeClass';
-        this.$span[method]('checked');
-    }
-
     // https://github.com/desandro/get-style-property/blob/master/get-style-property.js
     function getStyleProperty(propName) {
-        if (!propName) {
-            return;
-        }
+        var prefixes = 'Webkit Moz ms Ms O'.split(' '),
+            docElemStyle = document.documentElement.style;
 
-        // test standard property first
-        if (typeof docElemStyle[propName] === 'string') {
+        if (!propName)
+            return;
+
+        if (typeof docElemStyle[propName] === 'string')
             return propName;
-        }
 
         // capitalize
         propName = propName.charAt(0).toUpperCase() + propName.slice(1);
 
         // test vendor specific properties
         var prefixed;
+
         for (var i = 0, len = prefixes.length; i < len; i++) {
             prefixed = prefixes[i] + propName;
-            if (typeof docElemStyle[prefixed] === 'string') {
+            if (typeof docElemStyle[prefixed] === 'string')
                 return prefixed;
-            }
         }
     }
 
